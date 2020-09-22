400a0297fe408f0dd77b5be33530a3476ca682b1
elliott <furby_with_a_gun@hotmail.com>
2020-09-22 12:45:11 -0300

    worked on engine

--- RPG/RPG/Game.cpp
+++ RPG/RPG/Game.cpp
@@ -1,6 +1,13 @@
-//
-//
-//int main(int argc, char* args[])
-//{
-//    
-//}
+// Includes
+#include "gameEngine.h"
+
+// Global Constants
+const std::string GAME_TITLE = "RPG";
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+int main(int argc, char* args[])
+{
+    GameEngine engine = GameEngine(GAME_TITLE, SCREEN_WIDTH, SCREEN_HEIGHT);
+    engine.init();
+}
--- RPG/RPG/GameEngine.h
+++ RPG/RPG/GameEngine.h
@@ -1,18 +1,29 @@
 #pragma once
 #include <SDL.h>
+#include <SDL_ttf.h>
+#include <SDL_image.h>
+#include <string>
 
 class GameEngine
 {
     public:
-        GameEngine(int width, int height);
+        GameEngine(std::string title, int width, int height);
         ~GameEngine();
         bool init();
         void close();
+        void free();
+        SDL_Window* getMainWindow();
+        SDL_Renderer* getMainRenderer();
+        
 
     private:
         int screenWidth;
         int screenHeight;
-        SDL_Window* window;
-        SDL_Renderer* renderer;
+        SDL_Window* mainWindow;
+        SDL_Renderer* mainRenderer;
+        TTF_Font* mainFont;
+        std::string windowTitle;
+        SDL_Window* createWindow(const char* title, int height, int width);
+        SDL_Renderer* createRenderer(SDL_Window* window);
 };
 
